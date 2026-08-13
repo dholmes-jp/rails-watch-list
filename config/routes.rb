8c348b9930681fed2a74d3760f6a6f5f22d3d1fb
Drew Holmes <doryuu123@yahoo.co.jp>
2026-08-13 16:31:16 +0900

    Added controllers and models. Need to fix bugs in adding bookmarks/movies on lists.

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "lists", to: "lists#index", as: :lists
+  get "lists/new", to: "lists#new", as: :new_list
+  post "lists", to: "lists#create"
+  get "lists/:id", to: "lists#show", as: :list
+  get "lists/:list_id/bookmarks/new", to: "bookmarks#new", as: :new_list_bookmark
+  post "lists/:list_id/bookmarks", to: "bookmarks#create"
+  delete "bookmarks/:id", to: "bookmarks#destroy"
 end
